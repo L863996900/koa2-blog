803279cf22483e416dcde4663751b2e4d8fd5edd
Devon <863996900@qq.com>
2020-06-03 00:20:45 +0800

    添加token

---
+++ core/util.js
@@ -0,0 +1,50 @@
+// 静态方法存放
+
+// 遍历筛选某个类型 数据属性 返回数组 传参属性如下：
+// instance 类型
+// prefix 指定前缀
+// specifiedType 指定类型
+// filter 过滤条件
+// 返回数组
+const findMembers = function (instance ,{
+    prefix,
+    specifiedType,
+    filter
+}) {
+    // 递归
+    function _find(instance) {
+        if (instance._proto_ ===null){
+            return []
+        }
+        let names = Reflect.ownKeys(instance) // 参考es6 Reflect 此方法会返回一个数组，此数组中包含有参数对象自有属性名称。
+        names = names.filter((name)=>{ // filter过滤数组中不符合条件的属性并返回一个数组
+            // 过滤不满足条件的属性方法名
+            return shouldKeep(name) 
+        })
+
+        return[...name, ...find(instance._proto_)]
+    }
+    // 过滤条件
+    function shouldKeep(value){
+        if(filter) {
+            if(filter(value)){
+                return true
+            }
+        }
+        if(prefix){ //String.StartsWith 方法指定字符串是否以目标前缀开始，返回布尔值
+            if(value.startsWith(value)){
+                return true
+            }
+        }
+        if(specifiedType){
+            if(instance[value] instanceof specifiedType)
+            return true
+        }
+    }
+    
+    return _find(instance)
+}
+
+module.exports = {
+    findMembers
+}

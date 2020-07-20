00248d23d5bf946bc3dadd6f10632d037c151878
yongx.lee <863996900@qq.com>
2020-07-20 18:53:58 +0800

    注解

--- app/v2/upload.js
+++ app/v2/upload.js
@@ -11,6 +11,7 @@ var storage = multer.diskStorage({
     // destination: function (req, file, cb) {
     //     cb(null, 'public/uploads/')
     // },
+    //路径根据具体而定。如果不存在的话会自动创建一个路径
     destination: path.resolve('public/uploads/'),
     //修改文件名称
     filename: function (req, file, cb) {
